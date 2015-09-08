dfe7602219e54c949667cf5aed40d52fc34fe1a8
Christoph Massmann <c.massmann@vianetz.com>
2015-09-08 16:26:31 +0200

    RECO-296:  - separated code into folder structure  - added Recolize parameters for special page types  - cleaned everything up a bit

--- recolize.php
+++ recolize.php
@@ -1,93 +1,43 @@
 <?php
-/**
- * @package Recolize_RecommendationEngine
- * @version 1.0
- */
 /*
-Plugin Name: Recolize Recommendation Engine
-Plugin URI: http://wordpress.org/plugins/hello-dolly/
-Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. Whe
-n activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.
-Author: Recolize GmbH <service@recolize.com>
-Version: 1.0
-Author URI: http://www.recolize.com/
+Plugin Name: 	Recolize Recommendation Engine
+Plugin URI: 	http://www.recolize.com/
+Description: 	This plugin enables you to use the Recolize Recommendation Engine in your WordPress blog to offer a personalized blog experience.
+Version: 		1.0.0
+Author: 		Recolize GmbH <service@recolize.com>
+Author URI: 	http://www.recolize.com/
+License:		GNU General Public License Version 3 (GPLv3)
+License URI:	http://opensource.org/licenses/GPL-3.0
 */
 
-// Make sure we don't expose any info if called directly
+/**
+ * Recolize WordPress Plugin
+ *
+ * @section LICENSE
+ * This source file is subject to the GNU General Public License Version 3 (GPLv3).
+ *
+ * @category Recolize
+ * @package Recolize_RecommendationEngine
+ * @author Recolize GmbH <service@recolize.com>
+ * @copyright 2015 Recolize GmbH (http://www.recolize.com)
+ * @license http://opensource.org/licenses/GPL-3.0 GNU General Public License Version 3 (GPLv3).
+ */
+
 if (function_exists('add_action') === false) {
-	echo 'Hi there! I\'m just a plugin, not much I can do when called directly.';
-	exit;
+    exit;
 }
 
-/// Add Recolize JS Snippet to every page
-function addRecolizeJavaScriptSnippet()
-{
-    $options = get_option('RecolizeSettings');
-    echo $options['Recolize_textarea_field_0'];
-}
+require_once plugin_dir_path(__FILE__) . 'includes/PageType.php';
+require_once plugin_dir_path(__FILE__) . 'includes/JavaScriptSnippet.php';
+require_once plugin_dir_path(__FILE__) . 'admin/Settings.php';
 
-add_action('wp_head', 'addRecolizeJavaScriptSnippet');
+$pageType = new Recolize_PageType();
+$pageType->initialize();
 
-/// Recolize Admin Settings
-add_action('admin_menu', 'addRecolizeAdminMenu');
-add_action('admin_init', 'initRecolizeSettings');
+$javaScriptSnippet = new Recolize_JavaScriptSnippet();
+$javaScriptSnippet->initialize();
 
-function addRecolizeAdminMenu()
-{
-	add_options_page('Recolize Recommendation Engine', 'Recolize', 'manage_options', 'recolize', 'renderRecolizeOptionsPage');
-}
-
-function initRecolizeSettings()
-{
-	register_setting('pluginPage', 'RecolizeSettings');
-
-	add_settings_section(
-		'Recolize_pluginPage_section',
-		'<img src="http://www.recolize.com/skin/frontend/besugre/default/images/background_images/recolize_logo.png" />',
-		'callbackRecolizeSettingsSection',
-		'pluginPage'
-	);
-
-	add_settings_field(
-		'Recolize_textarea_field_0',
-		__('JS Snippet Code'),
-		'renderRecolizeJavaScriptSnippetTextareaField',
-		'pluginPage',
-		'Recolize_pluginPage_section'
-	);
-}
-
-
-function renderRecolizeJavaScriptSnippetTextareaField()
-{
-	$options = get_option('RecolizeSettings');
-	?>
-	<textarea cols='40' rows='5' name='RecolizeSettings[Recolize_textarea_field_0]'>
-		<?php echo $options['Recolize_textarea_field_0']; ?>
- 	</textarea>
-	<?php
-}
-
-
-function callbackRecolizeSettingsSection()
-{
-	echo __('Already registered for Recolize? Otherwise please create an account for free at <a href="#" target="_blank">our website</a>.');
-}
-
-
-function renderRecolizeOptionsPage()
-{
-	?>
-	<form action='options.php' method='post'>
-
-		<h2>Recolize Recommendation Engine</h2>
-
-		<?php
-		settings_fields('pluginPage');
-		do_settings_sections('pluginPage');
-		submit_button();
-		?>
-
-	</form>
-	<?php
+if (is_admin() === true) {
+    $settings = new Recolize_Admin_Settings();
+    $settings->initialize();
 }
